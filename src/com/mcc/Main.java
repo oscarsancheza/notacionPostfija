3c12505cde8f5b47d5f03b9b19d7a9fcbd41f0ee
oosancheza <oosancheza@concredito.com.mx>
2018-09-14 13:31:52 -0600

    [MOD] se agrego algoritmo para cambiar de infijo a postfijo

--- src/com/mcc/Main.java
+++ src/com/mcc/Main.java
@@ -1,7 +1,74 @@
 package com.mcc;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Stack;
+
 public class Main {
 
-    public static void main(String[] args) {
+  private static final String rutaArchivo =
+      "/home/dessis-aux23/Documentos/maestria/notacionPostfija/formulas.txt";
+
+  public static void main(String[] args) {
+
+    FileReader fileReader = null;
+    BufferedReader bufferedReader = null;
+
+    try {
+
+      fileReader = new FileReader(Main.rutaArchivo);
+      bufferedReader = new BufferedReader(fileReader);
+
+      String infijo;
+      String[] listadoInfijo;
+      StringBuilder postfijo;
+      Stack<String> operadores;
+
+      while ((infijo = bufferedReader.readLine()) != null) {
+        infijo = "(" + infijo + ")";
+        listadoInfijo = infijo.split("");
+        postfijo = new StringBuilder();
+
+        operadores = new Stack<>();
+        operadores.push("(");
+
+        for (String caracterActual : listadoInfijo) {
+
+          if (Character.isAlphabetic(caracterActual.charAt(0)) || caracterActual.equals("-")) {
+            postfijo.append(caracterActual);
+          } else {
+
+            if (caracterActual.equals(")")) {
+              while (!operadores.peek().equals("(")) {
+                postfijo.append(operadores.pop());
+              }
+              operadores.pop();
+            } else if (operadores.peek().equals("(") || caracterActual.equals("(")) {
+              operadores.push(caracterActual);
+            }
+          }
+        }
+
+        System.out.println(postfijo);
+      }
+
+    } catch (FileNotFoundException e) {
+      System.out.println("No se encontro el archivo en esa ruta. " + e.toString());
+    } catch (IOException e) {
+      System.out.println("Ha ocurrido un error." + e.toString());
+    } finally {
+      try {
+        if (fileReader != null) {
+          fileReader.close();
+        }
+        if (bufferedReader != null) {
+          bufferedReader.close();
+        }
+      } catch (IOException e) {
+        System.out.println("Ha ocurrido un error." + e.toString());
+      }
     }
+  }
 }
